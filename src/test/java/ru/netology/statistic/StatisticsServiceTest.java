6d5cb82b1434a63ce29a3d1919222f41a7924fc1
Maryust <88654209+Maryust@users.noreply.github.com>
2021-12-06 11:24:10 +0300

    Update StatisticsServiceTest.java

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class StatisticsServiceTest {
 
     @Test
-    void findMaxLastNumberinMassive() {
+    void findMaxLastNumberInMassive() {
         StatisticsService service = new StatisticsService();
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 13};
